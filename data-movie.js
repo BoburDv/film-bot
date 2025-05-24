6a88d6da8b269b0f88d1627f2fd33025f66dd5b7
BoburDv <exnessbbur@gmail.com>
2025-05-24 02:47:55 -0700

    film-bot

--- data-movie.js
+++ data-movie.js
@@ -1,7 +1,7 @@
 const films = {
   GarriPotter: {
-    1: 13,
-    2: 17,
+    1: 17,
+    2: 13,
     3: 22,
     4: 23,
     5: 26,
@@ -9,9 +9,19 @@ const films = {
     7: 28,
     8: 29,
   },
-  asalarich: {
+  Asalarich: {
     1: 30,
   },
+  OrgimchaOdam: {
+    1: 31,
+    2: 32,
+  },
+  Godzilla: {
+    1: 33,
+  },
+  Sonic: {
+    1: 36,
+  }
 };
 
 module.exports = films;
